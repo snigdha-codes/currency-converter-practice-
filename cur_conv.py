a464cef6961c515fabf94e6c3b6b52b326a18263
Snigdha <debnathsnigdha2001@gmail.com>
2025-02-08 11:12:58 +0530

    Add files via upload

---
+++ cur_conv.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def convert_currency(amount, from_currency, to_currency):
+    url = f"https://api.exchangerate-api.com/v4/latest/{from_currency.upper()}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        rates = response.json().get("rates", {})
+        if to_currency.upper() in rates:
+            converted = amount * rates[to_currency.upper()]
+            print(f"{amount} {from_currency.upper()} = {converted:.2f} {to_currency.upper()}")
+        else:
+            print(f"Error: {to_currency.upper()} not found in exchange rates.")
+    else:
+        print("Error fetching conversion rate")
+
+
+#Example Usage
+convert_currency(500, "EURO", "INR")
+
